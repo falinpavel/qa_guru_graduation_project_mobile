fe4b2a6498f7d2f2345e86640b2a908e704ec77a
falinpavel-macbook <aloverapf@yandex.ru>
2025-10-02 17:57:38 +0300

    rename test dirs

--- tests/tests_welcome_screen/test_skipped_welcome_promo.py
+++ tests/tests_welcome_screen/test_skipped_welcome_promo.py
@@ -1,6 +1,6 @@
 from helpers.mobile_application_manager.mobile_application_manager import kinopoisk_app
 
 
-class TestOpen:
+class TestSkippedWelcomePromo:
     def test_open(self):
         kinopoisk_app.welcome_screen.tap_button_next()
