d7702eb0aac7e8bacbcf8bd17860732df4c0a093
falinpavel-macbook <aloverapf@yandex.ru>
2025-10-04 23:48:15 +0300

    reformat tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,17 @@ from helpers.mobile_application_manager.mobile_application_manager import kinopo
 
 @pytest.fixture()
 def skipped_welcome_screen():
-    kinopoisk_app.welcome_screen.tap_button_next().tap_button_next().tap_yandex_plus_not_now_button().tap_allow_notifications_button()
+    kinopoisk_app.welcome_screen \
+        .tap_button_next() \
+        .tap_button_next() \
+        .tap_yandex_plus_not_now_button() \
+        .tap_allow_notifications_button()
 
 
 @pytest.fixture()
 def skipped_welcome_screen_without_network(disable_network):
-    kinopoisk_app.welcome_screen.tap_button_next().tap_button_next().tap_yandex_plus_widget_ok_button().tap_allow_notifications_button()
+    kinopoisk_app.welcome_screen \
+        .tap_button_next() \
+        .tap_button_next() \
+        .tap_yandex_plus_widget_ok_button() \
+        .tap_allow_notifications_button()
